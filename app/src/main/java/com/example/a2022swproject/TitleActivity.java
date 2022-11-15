51db18c292cf3f28cd63139bacafeb0d286de3b0
CSeoyeon <choiseoyeon0223@gmail.com>
2022-11-15 14:52:00 +0900

    splash activiy 추가 (toolbar 숨김)

--- app/src/main/java/com/example/a2022swproject/TitleActivity.java
+++ app/src/main/java/com/example/a2022swproject/TitleActivity.java
@@ -12,7 +12,7 @@ public class TitleActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_title);
-
+        getSupportActionBar().hide();
         splash(1);
 
     }
